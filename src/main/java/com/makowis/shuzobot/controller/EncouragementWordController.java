23c5c9a96387c0147523654d44ec312e1fc26500
Makoto Henmi <mako.mail.g@gmail.com>
2017-09-21 01:06:03 +0900

    追加画面の大枠を作成

--- src/main/java/com/makowis/shuzobot/controller/EncouragementWordController.java
+++ src/main/java/com/makowis/shuzobot/controller/EncouragementWordController.java
@@ -3,13 +3,17 @@ package com.makowis.shuzobot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import com.makowis.shuzobot.dao.EncouragementWordDao;
 import com.makowis.shuzobot.entity.EncouragementWord;
+import com.makowis.shuzobot.form.EncouragementWordAddForm;
 
 @Controller
 @RequestMapping("/encouragementWords")
@@ -23,4 +27,20 @@ public class EncouragementWordController {
         model.addAttribute("encouragementWords", encouragementWords);
         return "encouragementWords/list";
     }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add(@ModelAttribute EncouragementWordAddForm encouragementWordAddForm, Model model) {
+        return "encouragementWords/add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String doAdd(@Validated @ModelAttribute EncouragementWordAddForm encouragementWordAddForm,
+            BindingResult result, Model model, @AuthenticationPrincipal User user) {
+
+        if (!result.hasErrors()) {
+            model.addAttribute("successMessage", String.format("「%s」を追加しました。", encouragementWordAddForm.getWord()));
+        }
+        return "encouragementWords/add";
+    }
+
 }
